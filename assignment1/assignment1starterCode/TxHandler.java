7c387eebbc6910b4726825857228fce23f182356
Archies09 <adfzr007@gmail.com>
2018-01-12 17:08:40 +0530

    Optimized TxHandler Implementation

--- assignment1/assignment1starterCode/TxHandler.java
+++ assignment1/assignment1starterCode/TxHandler.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 public class TxHandler {
 
@@ -62,15 +60,95 @@ public class TxHandler {
         return true;
     }
 
+    HashMap<Transaction,HashSet<Transaction> > dependencyGraph = new HashMap<Transaction,HashSet<Transaction> >();
+    HashSet<Transaction> possibleTxsSet = new HashSet<Transaction>();
+    List<Transaction> topoOrder = new ArrayList<Transaction>();
+    
+    void topoOrdering(Transaction t, HashMap<Transaction,Boolean > visitedMap, Stack<Transaction> stackOfTx)
+    {
+    	visitedMap.put(t,true);
+    	Transaction r;
+    	
+    	Iterator<Transaction> txIterator = dependencyGraph.get(t).iterator();
+    	while(txIterator.hasNext())
+    	{
+    		r=txIterator.next();
+    		if(visitedMap.get(r)==null)
+    			topoOrdering(r, visitedMap, stackOfTx);
+    	}
+    	
+    	stackOfTx.push(t);
+    }
+    
+    void findTopoSort()
+    {
+    	Stack<Transaction> stackOfTx = new Stack<Transaction>();
+    	HashMap<Transaction,Boolean> visitedMap = new HashMap<Transaction,Boolean>();
+    	for(Transaction t : possibleTxsSet)
+    	{
+    		if(visitedMap.get(t)==null)
+    			topoOrdering(t,visitedMap,stackOfTx);
+    	}
+    	
+    	while(!stackOfTx.empty())
+    	{
+    		topoOrder.add(stackOfTx.pop());
+    	}
+    }
+    
+    
+    
     /**
      * Handles each epoch by receiving an unordered array of proposed transactions, checking each
      * transaction for correctness, returning a mutually valid array of accepted transactions, and
      * updating the current UTXO pool as appropriate.
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
+	    
+    possibleTxsSet.addAll(Arrays.asList(possibleTxs));	
+    for(Transaction t : possibleTxs)
+    {
+    	dependencyGraph.put(t, new HashSet<Transaction>());
+    	
+    	for(int i=0;i<t.numOutputs();i++)
+    	{
+    		UTXO testUTXO = new UTXO(t.getHash(),i);
+	    	
+    		Transaction qDepend=null;
+    		
+    		int testFlag=0;
+    		
+    		for(Transaction q : possibleTxs)
+    	    {
+    			for(int j=0;j<q.numInputs();j++)
+    	    	{
+    				UTXO qUTXO = new UTXO(q.getInput(j).prevTxHash,q.getInput(j).outputIndex);
+    				if(qUTXO.equals(testUTXO))
+    				{
+    					testFlag=1;
+    					qDepend=q;
+    					dependencyGraph.get(t).add(qDepend);
+    				}
+    			}
+    	    }
+    		
+    	}
+    	
+    }
+    
+    System.err.println("Dependency Graph is:");
+    for(Transaction t : possibleTxs)
+    {
+    	System.err.println(t+" : "+dependencyGraph.get(t));
+    }
+    
 	
-	List<Transaction> maximalSetOfValidTransaction = new ArrayList<>();
-	for(Transaction t : possibleTxs)
+    findTopoSort();
+    
+    System.err.println(topoOrder);
+    List<Transaction> maximalSetOfValidTransaction = new ArrayList<>();
+    
+	for(Transaction t : topoOrder)
 	{
 		if(isValidTx(t))
 		{
@@ -79,13 +157,13 @@ public class TxHandler {
 
 			for(int i=0;i<t.numInputs();i++)
 	        	{
-				publicLedger.removeUTXO(new UTXO(t.getInput(i).prevTxHash,t.getInput(i).outputIndex));
-			}
+					publicLedger.removeUTXO(new UTXO(t.getInput(i).prevTxHash,t.getInput(i).outputIndex));
+	        	}
 		
 			for(int i=0;i<t.numOutputs();i++)
-                	{
-                        	publicLedger.addUTXO(new UTXO(t.getHash(),i),t.getOutput(i));
-                	}	
+	        	{
+	               	publicLedger.addUTXO(new UTXO(t.getHash(),i),t.getOutput(i));
+	        	}	
 			
 		}
 
